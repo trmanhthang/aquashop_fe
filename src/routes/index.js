aa63591986707c1d2e81e56f63054fb7e6c762af
Thắng Trương <manhthang080698@gmail.com>
2024-06-23 21:34:58 +0700

    config project

---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+import Home from "~/pages/Home";
+import Login from "~/pages/Login";
+
+
+const publicRoutes = [
+    { path: '/', component: Home},
+    { path: '/login', component: Login }
+]
+
+const privateRoutes = [
+
+]
+
+export { publicRoutes, privateRoutes }
---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+var path = require('path');
+
+module.exports = {
+    resolve: {
+        extensions: ['.js', '.json', '.vue'],
+        alias: {
+            '~': path.resolve(__dirname, '../../../utils/MyUtilFn'),
+        },
+    },
+};
